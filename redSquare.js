b9a69d105f2aa01d36e17c014456a2e3f35a04a6
nul00000000 <43396111+nul00000000@users.noreply.github.com>
2018-09-20 15:28:00 -0400

    RedSquare files

---
+++ redSquare.js
@@ -0,0 +1,105 @@
+var context, controller, rectangle, loop;
+var width, height;
+
+context = document.querySelector("canvas").getContext("2d");
+
+width = innerWidth - 20;
+height = innerHeight - 20;
+
+context.canvas.height = height;
+context.canvas.width = width;
+
+rectangle = {
+	
+	height:32,
+	jumping:true,
+	width:32,
+	x:width / 2,
+	dx:0,
+	y:0,
+	dy:0
+	
+};
+
+controller = {
+	
+	left:false,
+	right:false,
+	up:false,
+	keyListener:function(event) {
+		
+		var keyState = event.type == "keydown";
+		
+		switch(event.keyCode) {
+			
+			case 37:
+				controller.left = keyState;
+				break;
+			case 38:
+				controller.up = keyState;
+				break;
+			case 39:
+				controller.right = keyState;
+				break;
+		}
+		
+	}
+	
+};
+
+loop = function() {
+	
+	if(controller.up) {
+		rectangle.dy -= 1;
+		rectangle.jumping = true;
+	}
+	
+	if(controller.left) {
+		rectangle.dx -= 0.5;
+	}
+	
+	if(controller.right) {
+		rectangle.dx += 0.5;
+	}
+	
+	rectangle.dy += 0.5;
+	rectangle.x += rectangle.dx;
+	rectangle.y += rectangle.dy;
+	rectangle.dx *= 0.99;
+	rectangle.dy *= 0.99;
+	
+	if(rectangle.y > height - 32) {
+		rectangle.jumping = false;
+		rectangle.y = height - 32;
+		rectangle.dy = 0;
+	}
+	
+	if(rectangle.y < 0) {
+		rectangle.y = 0;
+		rectangle.dy = 0;
+	}
+	
+	if(rectangle.x < -32) {
+		rectangle.x = width;
+	} else if(rectangle.x > width) {
+		rectangle.x = -32;
+	}
+	
+	context.fillStyle = "#202020";
+	context.fillRect(0, 0, width, height);
+	context.fillStyle = "#ff0000";
+	context.fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+	window.requestAnimationFrame(loop);
+		
+};
+
+window.addEventListener("keydown", controller.keyListener);
+window.addEventListener("keyup", controller.keyListener);
+window.requestAnimationFrame(loop);
+
+
+
+
+
+
+
